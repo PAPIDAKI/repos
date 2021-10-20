595721d77b1524c519c10bc91c1e405289425415
PAPIDAKI <papidakis@gmail.com>
2021-10-20 13:01:26 +0300

    Fill up the dashboard values

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,14 +2,13 @@ class ReportsController < ApplicationController
 
   def dashboard
     @current_date = Date.today 
-    @last_invoice = " Last invoice was by SM  dated 13/10/2021"
-    @next_30_forcast = "For the next 30 days we will be needing 40 tons distributed to the following custommers"
-    @last_year_sales = "Last year from 2019 to 2020  Year to day 500 tons  "
-    @current_year_sales = "Current year sales from one year to the day today "
+    @last_invoice = Sale.order(:date).last.date
+    @next_30_forcast = Sale.raisins.next_30.total_kgs/1000
+    @last_year_sales = Sale.ly.raisins.total_kgs/1000
+    @current_year_sales = Sale.cy.raisins.total_kgs/1000
     @till_new_crop = Date.new(2022,9,1)-Date.today
 
   end
-
 
 
 
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,47 @@
 class Sale < ApplicationRecord
+  
+  #product
+  scope :raisins,->{where(code: ['1011','1014']) }
+  scope :raisin_imports,->{where("afm LIKE ?",'999999999%') }
+  scope :raisin_buys,->{where(code:['2012','2022'])} 
+
+  scope :grape_sales,->{where(code: ['1012','1015','1042'])}
+  scope :grape_x,->{where(code:['3023'])}
+  scope :raisins_x,->{where(code:['3021'])}
+  # fason  rents 
+
+
+  #time range
+  scope :cy,->{where(date:Time.now-1.year..Time.now)}
+  scope :ly,->{where(date:Time.now-2.year..Time.now-1.year)}
+  scope :ty,->{where(date:Time.now-3.year..Time.now-2.year)}
+
+  scope :next_30,->{where(date:Time.now-1.year..Time.now-11.months)} #next_30
+  scope :next_,->(d){where(date:Time.now-1.year..Time.now-365.days+d.days)}
+
+  # last invoice date  
+  scope :most_recent,->{
+    first.date
+  }
+
+  #problems 
+  scope :raisin_returns, -> {
+    where('code = ? OR code = ? AND kgs != ? ' ,1311,1321,0)
+  }
+    # remove pallet - and zero s kgs zeros 
+  scope :palet_free, -> {
+  }
+
+  def self.total_kgs
+    sum{|invoice| invoice.kgs}
+  end
+
+  def self.total_value
+    sum{|inv| inv.value}
+  end
+
+  def self.avg_price
+    total_value/total_kgs
+  end
 end
+
